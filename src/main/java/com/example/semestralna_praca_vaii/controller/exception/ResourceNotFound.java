e2cdf0f2b634bc64620b68c7d5ee73aae968371c
jurajgajdiciar <juraj.gajdiciar9@gmail.com>
2024-11-30 10:29:25 +0100

    init resourceNotFound

---
+++ src/main/java/com/example/semestralna_praca_vaii/controller/exception/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.example.semestralna_praca_vaii.controller.exception;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+}
