0c50831ae68d139b942692b1f3115a5bc38b948d
jurajgajdiciar <juraj.gajdiciar9@gmail.com>
2025-01-11 15:34:54 +0100

    init customerOrder repository

--- src/main/java/com/example/semestralna_praca_vaii/data/customerOrder/CustomerOrder.java
+++ src/main/java/com/example/semestralna_praca_vaii/data/customerOrder/CustomerOrder.java
@@ -1,9 +1,138 @@
 package com.example.semestralna_praca_vaii.data.customerOrder;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.semestralna_praca_vaii.data.customerServices.CustomerServices;
+import com.example.semestralna_praca_vaii.data.person.Person;
+import com.example.semestralna_praca_vaii.data.vehicle.Vehicle;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "customerOrder")
 public class CustomerOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "orderId", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "email", referencedColumnName = "email", nullable = false)
+    private Person person;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle")
+    private Vehicle vehicle;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "serviceId", nullable = false)
+    private CustomerServices serviceid;
+
+    @NotNull
+    @Column(name = "dateOfCreate", nullable = false)
+    private LocalDate dateofcreate;
+
+    @NotNull
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Size(max = 255)
+    @Column(name = "price")
+    private String price;
+
+    @Column(insertable=false, updatable=false)
+    private String serviceId;
+
+    @Column(insertable=false, updatable=false)
+    private String vehiclePlateNumber;
+
+    @Column(insertable=false, updatable=false)
+    private String email;
+
+    public String getVehiclePlateNumber() {
+        return vehiclePlateNumber;
+    }
+
+    public void setVehiclePlateNumber(String vehiclePlateNumber) {
+        this.vehiclePlateNumber = vehiclePlateNumber;
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public @NotNull Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(@NotNull Person person) {
+        this.person = person;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDate getDateofcreate() {
+        return dateofcreate;
+    }
+
+    public void setDateofcreate(LocalDate dateofcreate) {
+        this.dateofcreate = dateofcreate;
+    }
+
+    public CustomerServices getServiceid() {
+        return serviceid;
+    }
+
+    public void setServiceid(CustomerServices serviceid) {
+        this.serviceid = serviceid;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Person getEmail() {
+        return person;
+    }
+
+    public void setEmail(Person person) {
+        this.person = person;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/example/semestralna_praca_vaii/data/customerOrder/CustomerRepository.java
+++ src/main/java/com/example/semestralna_praca_vaii/data/customerOrder/CustomerRepository.java
@@ -1,4 +1,20 @@
 package com.example.semestralna_praca_vaii.data.customerOrder;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
 public interface CustomerRepository {
+
+    Page<CustomerOrder> findAll();
+
+    Optional<CustomerOrder> findById(Integer id);
+
+    boolean existsById(Integer id);
+
+    @Query("SELECT c FROM CustomerOrder c WHERE c.person.email = :email")
+    Page<CustomerOrder> findByEmail(String email);
 }
