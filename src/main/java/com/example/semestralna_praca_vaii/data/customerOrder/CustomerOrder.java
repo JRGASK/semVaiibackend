94f4bad878a4eb257cc91a24aa6d88a1670eff6c
jurajgajdiciar <juraj.gajdiciar9@gmail.com>
2025-01-11 14:12:02 +0100

    init customerOrder

---
+++ src/main/java/com/example/semestralna_praca_vaii/data/customerOrder/CustomerOrder.java
@@ -0,0 +1,9 @@
+package com.example.semestralna_praca_vaii.data.customerOrder;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customerOrder")
+public class CustomerOrder {
+}
---
+++ src/main/java/com/example/semestralna_praca_vaii/data/customerOrder/CustomerRepository.java
@@ -0,0 +1,4 @@
+package com.example.semestralna_praca_vaii.data.customerOrder;
+
+public interface CustomerRepository {
+}
